06e46803491adc55645a6b8fd62a2901dbd7f394
Mjchaurasia9 <92732297+Mjchaurasia9@users.noreply.github.com>
2022-04-28 17:39:37 +0530

    Add files via upload

---
+++ charReverse/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Enter a string:\t");
+string str = Console.ReadLine();
+
+char [] ch = new char [str.Length];
+for (int i = 0; i < str.Length; i++)
+{
+ch[i] = str[i];
+}
+
+Array.Reverse(ch);
+
+foreach (char i in ch)
+{
+Console.Write(i+" ");
+}
